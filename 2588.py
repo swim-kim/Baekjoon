53592bac4537dc0e21f349cbc4555a35a3c8db81
swimkim <81215099+swim-kim@users.noreply.github.com>
2023-01-13 22:44:17 +0900

    Create 2588.py
    2588

---
+++ 2588.py
@@ -0,0 +1,18 @@
+a = int(input())
+b = input()
+
+a1 = a*int(b[2])
+a2 = a*int(b[1])
+a3 = a*int(b[0])
+a4 = a*int(b)
+
+print(a1,a2,a3,a4, sep ='\n')
+
+
+
+
+""" a,b = map(int,input().split('/n'))
+print((b%10)*a)
+print((b%100)//10*a)
+print((b//100)*a)
+print(a*b) """
