2dab622a3ee687bf7395a8e1e940ee8b663ab974
swimkim <81215099+swim-kim@users.noreply.github.com>
2023-01-30 22:31:43 +0900

    기본수학1

--- step7/벌집.py
+++ step7/벌집.py
@@ -1,9 +1,12 @@
 num = int(input())
-i=0
-if num==1:
-    print(1)
-elif num>=2 and num<=7:
-    print(2)
-else:
-    if 2<num%6 and num%6<7:
-        print(int(num/6))
+
+cnt=1
+count=6
+count_room=1
+
+while num > cnt:
+    count_room+=1
+    cnt+=count
+    count+=6
+print(count_room)
+
---
+++ step7/분수찾기.py
@@ -0,0 +1,15 @@
+n = int(input())
+
+count = 1
+cnt =1
+
+cnt_m=cnt
+cnt_s=1
+while n > count:
+    cnt+=1
+    cnt+=count
+    count+=1
+print (cnt)
+
+
+
